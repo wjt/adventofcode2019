7da6ee21ac67998c1ffa427dbab4a61d7575968e
Will Thompson <will@willthompson.co.uk>
2019-12-08 22:03:19 +0000

    day 8 part 2

--- src/bin/day8/main.rs
+++ src/bin/day8/main.rs
@@ -2,8 +2,22 @@ use itertools::Itertools;
 use std::env;
 use std::io::{self, BufRead};
 
-fn count_digit(layer: &Vec<char>, c: char) -> usize {
-    layer.iter().filter(|&d| *d == c).count()
+fn format_layer(layer: &str, width: usize) {
+    for line in &layer.chars().chunks(width) {
+        print!("  ");
+        for c in line {
+            if c == '1' {
+                print!(".")
+            } else {
+                print!(" ");
+            }
+        }
+        println!("");
+    }
+}
+
+fn count_digit(layer: &str, c: char) -> usize {
+    layer.chars().filter(|&d| d == c).count()
 }
 
 fn main() {
@@ -17,18 +31,29 @@ fn main() {
         .map(|l| l.unwrap())
         .collect::<Vec<String>>()
         .join("");
-    let layers: Vec<Vec<char>> = line
+    let layers: Vec<String> = line
         .chars()
         .chunks(width * height)
         .into_iter()
-        .map(|layer| layer.collect::<Vec<char>>())
+        .map(|layer| layer.collect::<String>())
         .collect();
     let fewest_0_digits = layers
         .iter()
         .min_by_key(|layer| count_digit(layer, '0'))
         .unwrap();
+    format_layer(fewest_0_digits, width);
     println!(
         "{}",
         count_digit(fewest_0_digits, '1') * count_digit(fewest_0_digits, '2')
     );
+
+    let zero: String = "2".repeat(width * height);
+    let composited = layers.iter().fold(zero, |upper, lower| {
+        upper
+            .chars()
+            .zip_eq(lower.chars())
+            .map(|(c, d)| if c == '2' { d } else { c })
+            .collect::<String>()
+    });
+    format_layer(&composited, width);
 }
