982227c02b3c1433ec74a3e4eb1eddc7c1d85728
Will Thompson <will@willthompson.co.uk>
2019-12-08 21:36:03 +0000

    day 7 part 2

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -12,6 +12,12 @@ struct VM {
     output: IO,
 }
 
+enum State {
+    Running,
+    Halted,
+    NeedInput,
+}
+
 impl VM {
     fn new(program: &Memory, input: IO) -> Self {
         VM {
@@ -42,15 +48,22 @@ impl VM {
         self.ip += 4
     }
 
-    fn step(&mut self) -> bool {
+    fn step(&mut self) -> State {
         let instruction = self.memory[self.ip];
         match instruction % 100 {
             1 => self.int3(|a, b| a + b),
             2 => self.int3(|a, b| a * b),
             3 => {
                 let j = self.memory[self.ip + 1];
-                self.memory[j as usize] = self.input.pop_front().expect("more input");
-                self.ip += 2
+                match self.input.pop_front() {
+                    Some(val) => {
+                        self.memory[j as usize] = val;
+                        self.ip += 2;
+                    }
+                    None => {
+                        return State::NeedInput;
+                    }
+                }
             }
             4 => {
                 self.output.push_back(self.arg(1));
@@ -72,21 +85,19 @@ impl VM {
             }
             7 => self.int3(|a, b| (a < b) as i64),
             8 => self.int3(|a, b| (a == b) as i64),
-            99 => return false,
+            99 => return State::Halted,
             x => panic!("the discotheque: ip {}: {}", self.ip, x),
         }
-        true
+        State::Running
     }
 
-    fn run(&mut self) {
-        // println!("--> {:?}", self.memory);
+    fn run(&mut self) -> State {
         loop {
-            if !self.step() {
-                break;
+            match self.step() {
+                State::Running => {}
+                x => return x,
             }
-            // println!("--- {:?}", self.memory);
         }
-        // println!("<-- {:?}", self.memory);
     }
 }
 
@@ -97,14 +108,14 @@ mod tests {
     #[test]
     fn test_day2() {
         let program = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
-        let input = Vec::new();
+        let input = VecDeque::new();
         let mut vm = VM::new(&program, input);
         vm.run();
 
         assert_eq!(vm.memory, vec![30, 1, 1, 4, 2, 5, 6, 0, 99]);
     }
 
-    fn run_with_input(program: &Memory, input: Memory) -> (Memory, Memory) {
+    fn run_with_input(program: &Memory, input: IO) -> (Memory, IO) {
         let mut vm = VM::new(&program, input);
         vm.run();
 
@@ -114,9 +125,9 @@ mod tests {
     #[test]
     fn test_day5() {
         let program = vec![3, 3, 1107, -1, 8, 3, 4, 3, 99];
-        let input = vec![7];
+        let input = IO::from(vec![7]);
         let (_, output) = run_with_input(&program, input);
-        assert_eq!(output, vec![1]);
+        assert_eq!(output, IO::from(vec![1]));
     }
 }
 
@@ -135,6 +146,37 @@ fn amplify(program: &Memory, sequence: Vec<i64>) -> i64 {
     input.pop_front().expect("at least one output")
 }
 
+fn amplify_loop(program: &Memory, sequence: Vec<i64>) -> i64 {
+    let mut vms: Vec<VM> = sequence
+        .iter()
+        .map(|phase| {
+            let input = IO::from(vec![*phase]);
+            VM::new(&program, input)
+        })
+        .collect();
+    vms[0].input.push_back(0);
+    let n = vms.len();
+    loop {
+        for i in 0..n {
+            let (result, mut output): (State, IO) = {
+                let vm = &mut vms[i];
+                let r = vm.run();
+                let o = vm.output.drain(..).collect();
+                (r, o)
+            };
+            if let State::Halted = result {
+                if i == n - 1 {
+                    return output
+                        .pop_front()
+                        .expect("at least one output when final machine halts");
+                }
+            }
+            let next = &mut vms[(i + 1) % n];
+            next.input.append(&mut output);
+        }
+    }
+}
+
 fn main() {
     let stdin = io::stdin();
     let handle = stdin.lock();
@@ -151,4 +193,11 @@ fn main() {
         .max()
         .expect("at least one element");
     println!("{}", part1);
+
+    let part2 = (5..10)
+        .permutations(5)
+        .map(|sequence| amplify_loop(&program, sequence))
+        .max()
+        .expect("at least one element");
+    println!("{}", part2);
 }
